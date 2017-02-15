cf557778194473613d3dfd274b4b41ebeee50472
Dwi Ardi Irawan <dwiardiirawan@gmail.com>
2017-02-15 21:14:21 +0700

    add 2 files, CompareTheTriplets.java and HelloWorld.java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.dwiardiirawan.challenges;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String inputString = scan.nextLine();
+		scan.close();
+		
+		System.out.println("Hello World");
+		System.out.println(inputString);
+
+	}
+
+}
