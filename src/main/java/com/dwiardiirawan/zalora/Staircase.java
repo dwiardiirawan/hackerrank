b7ff7379ee22345768fd815fe7eb252d960b8d1a
Dwi A. Irawan <d.irawan@allegrodevelopment.com>
2017-02-16 22:23:40 +0700

    zalora, exercise

---
+++ src/main/java/com/dwiardiirawan/zalora/Staircase.java
@@ -0,0 +1,23 @@
+package com.dwiardiirawan.zalora;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for (int i = n; i > 0; i--) {
+			for (int j = 1; j <= n; j++) {
+				if(i>j)
+					System.out.print(" ");
+				else
+					System.out.print("*");
+				
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
---
+++ src/main/java/com/dwiardiirawan/zalora/SumThemAll.java
@@ -0,0 +1,18 @@
+package com.dwiardiirawan.zalora;
+
+import java.util.Scanner;
+
+public class SumThemAll {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum+= in.nextInt();
+		}
+		System.out.println(sum);
+		in.close();
+	}
+
+}
